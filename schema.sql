83755ef21a5b0e0b2cbd237955046f38474e1307
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-06 09:03:07 +0100

    Create a table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+id int PRIMARY KEY NOT NULL,
+name varchar(40) NOT NULL,
+date_of_birth date NOT NULL,
+escape_attempts int,
+neutered bit,
+weight decimal
 );
