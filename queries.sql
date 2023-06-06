06be53d243fa92aba6260519cb0dbe7ad6778dd9
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-06 23:31:43 +0100

    Write queries to answer some questions

--- queries.sql
+++ queries.sql
@@ -52,3 +52,24 @@ SET weight_kg = weight_kg * -1
 WHERE weight_kg < 0
 
 COMMIT;
+
+SELECT COUNT(*) FROM animals; 
+
+SELECT COUNT(escape_attempts) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered,SUM(escape_attempts)
+FROM animals
+GROUP BY neutered
+ORDER BY SUM(escape_attempts) DESC;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) 
+from animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
