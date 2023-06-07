fb10076cd8b2c289c02c4a057a2108600980710e
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-07 15:46:42 +0100

    Create new tables and modify the existing table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,34 @@ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 
 ALTER TABLE animals
 ADD COLUMN species varchar(40);
+
+
+CREATE TABLE owners(
+id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+full_name varchar(40) NOT NULL,
+age int
+);
+
+CREATE TABLE species(
+id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+name varchar(40)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id int;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id int;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
